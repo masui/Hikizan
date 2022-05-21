641c319ac4f895ced48b0a71eb2a7ab7f17a9e93
Toshiyuki Masui <masui@pitecan.com>
2022-05-21 09:27:26 +0900

    Scrapbox上にコードを移動

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+# Scrapboxからコードを取得
+# -r オプションで上書き
+get:
+	wget -q -r https://scrapbox.io/api/code/Hikizan/index.html/index.html
+	wget -q -r https://scrapbox.io/api/code/Hikizan/hikizan.css/hikizan.css
+	wget -q -r https://scrapbox.io/api/code/Hikizan/hikizan.js/hikizan.js
+
 push:
 	git commit -a -m '...' ; git push
--- hikizan.js
+++ hikizan.js
@@ -2,33 +2,11 @@
 var minuend, subtrahend, difference;
 var munuendlist, subtrahendlist;
 
-function runx(){
-    minuend = document.querySelector('#minuend')
-    subtrahend = document.querySelector('#subtrahend')
-    difference = document.querySelector('#difference')
-
-    difference.addEventListener('click',function(){
-	alert(100)
-    })
-    
-    minuend_list = document.getElementById('minuendlist')
-    var li = document.createElement('li')
-    var a = document.createElement('a')
-    a.href = 'http://example.com'
-    a.innerHTML = "aaaa"
-    li.appendChild(a)
-    minuendlist.appendChild(li);
-    //li.remove();
-}
-
 function handleDDFile(file){
     var li = $('<li>').appendTo(minuendlist)
     var a = document.createElement('a')
     a.href = 'http://example.com'
     console.log(file)
-    //a.innerHTML = file.name
-    //li.append(a)
-    //minuendlist.append(li);
 }
 
 function run(){
@@ -44,31 +22,9 @@ function run(){
     //li.remove();
 
     // Drag&Dropされたファイルの処理
-    /*
-    minuend.bind("dragover", (e) => {
-	return false;
-    }).bind("dragend", (e) => {
-	return false;
-    }).bind("drop", (e) => {
-	e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
-	//console.log(e.originalEvent.dataTransfer)
-	let dt = e.originalEvent.dataTransfer
-	let li = $('<li>').appendTo(minuendlist)
-	let a = $('<a>').appendTo(li)
-	if(dt.files[0]){ // ファイルのD&D
-	    a.text(dt.files[0].name)
-	    a.attr('href','http://example.com')
-	}
-	else {
-	    var url = dt.getData("URL")
-	    a.text(url)
-	    a.attr('href',url)
-	}
-    })
-    */
-    minuend.on("dragover", (e) => { return false } )
-    minuend.on("dragend", (e) => { return false; } )
-    minuend.on("drop", (e) => {
+    //minuend.on("dragover", e => { return false } )
+    //minuend.on("dragend", e => { return false; } )
+    minuend.on("drop", e => {
 	e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
 	//console.log(e.originalEvent.dataTransfer)
 	let dt = e.originalEvent.dataTransfer
