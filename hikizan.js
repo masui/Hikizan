d24dc994015afd724b41844385856c3eb2b360a7
Toshiyuki Masui <masui@pitecan.com>
2022-05-22 22:59:49 +0900

    ...

--- hikizan.js
+++ hikizan.js
@@ -1,13 +1,3 @@
-//$ = require('jquery')
-//process = require('process') // 何故かこれがないとエラーになる
-//kuromoji = require('kuromoji')
-
-// kuromojiテスト
-//kuromoji.builder({ dicPath: "dict/" }).build(function (error, tokenizer) {
-//    var path = tokenizer.tokenize("私の名前は増井です");
-//    console.log(path);
-//});
- 
 var minuend, subtrahend, difference;
 var munuendlist, subtrahendlist;
 
