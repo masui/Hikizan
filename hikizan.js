ea63ffc8e4bcb6a9599fc7fbba4ed4e736aa06fb
Toshiyuki Masui <masui@pitecan.com>
2022-05-22 10:10:55 +0900

    ...

--- hikizan.js
+++ hikizan.js
@@ -1,9 +1,8 @@
 $ = require('jquery')
 process = require('process') // 何故かこれがないとエラーになる
 kuromoji = require('kuromoji')
-alert(__dirname + "/node_modules/kuromoji/dict/")
 
-kuromoji.builder({ dicPath: "node_modules/kuromoji/dict/" }).build(function (error, tokenizer) {
+kuromoji.builder({ dicPath: "dict/" }).build(function (error, tokenizer) {
      var path = tokenizer.tokenize("私の名前は増井です");
     console.log(path);
 });
