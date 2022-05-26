e83a9aa7eef3be74eaf223575d9444ae7804d002
Toshiyuki Masui <masui@pitecan.com>
2022-05-26 20:35:55 +0900

    Drag&Drop関連のコードを削除

--- hikizan.js
+++ hikizan.js
@@ -1,5 +1,4 @@
 let minuend, subtrahend, difference
-// var munuendlist, subtrahendlist;
 
 function run () {
   minuend = $('#minuend') // 減算されるデータのtextarea
@@ -14,6 +13,7 @@ function run () {
     // kuromoji.builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict" }).build(function (error, tokenizer) {
     // 仕方がないのでdictファイルもサイトに置いたらhikizan.orgで動いた 2022/5/23 11:10
     kuromoji.builder({ dicPath: './dict' }).build(function (error, tokenizer) {
+      console.log(error) // ESLint対策
       let tokens = tokenizer.tokenize(minuend.val())
       tokens.forEach(word => {
         a[word.surface_form] = 1
@@ -51,39 +51,6 @@ function run () {
       $('#difference').css('background-color', '#fff')
     })
   })
-
-  /*
-  // URLやデータファイルをDrag&Dropで指定する予定 (未実装 2022/5/23)
-  minuendlist = $('#minuendlist')
-  li = $('<li>').appendTo(minuendlist)
-  $('<a>').text('aaaa').attr('href','http://example.com').appendTo(li)
-  //li.remove();
-
-  // Drag&Dropされたファイルの処理
-  //minuend.on("dragover", e => { return false } ) 不要っぽい
-  //minuend.on("dragend", e => { return false; } )
-  minuend.on('drop', e => {
-    e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
-    //console.log(e.originalEvent.dataTransfer)
-    let dt = e.originalEvent.dataTransfer
-    let li = $('<li>').appendTo(minuendlist)
-    let a = $('<a>').appendTo(li)
-    a.attr('class','list')
-    if(dt.files[0]) { // ファイルのD&D
-      a.text(dt.files[0].name)
-      a.attr('href','http://example.com')
-    }
-    else { // URLのD&D
-      var url = dt.getData('URL')
-      a.text(url)
-      a.attr('href',url)
-    }
-  })
-
-  $('body').on('drop', function (e) {
-    e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
-  })
-  */
 }
 
 $(function () {
