8fde97907372119e8c86c71d37e013af6c6013ba
Toshiyuki Masui <masui@pitecan.com>
2022-05-26 15:04:49 +0900

    ...

--- hikizan.js
+++ hikizan.js
@@ -14,31 +14,30 @@ function run () {
     // kuromoji.builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict" }).build(function (error, tokenizer) {
     // 仕方がないのでdictファイルもサイトに置いたらhikizan.orgで動いた 2022/5/23 11:10
     kuromoji.builder({ dicPath: './dict' }).build(function (error, tokenizer) {
-      var tokens = tokenizer.tokenize(minuend.val())
+      let tokens = tokenizer.tokenize(minuend.val())
       tokens.forEach(word => {
         a[word.surface_form] = 1
       })
 
       // 辞書を引算するような場合にkuromojiを適用しないことにする
-      var useKuromoji = false
-      var subdata = subtrahend.val()
-      if (subdata.length < 10000){
+      let useKuromoji = false
+      const subdata = subtrahend.val()
+      if (subdata.length < 10000) {
         useKuromoji = true
-      }
-      else {
-        const lines = subdata.split("\n")
+      } else {
+        const lines = subdata.split('\n')
         lines.forEach(line => {
           const words = line.split(/\s+/)
           words.forEach(word => {
-            if(word.length > 10){
-               useKuromoji = true
+            if (word.length > 10) {
+              useKuromoji = true
             }
             delete a[word]
           })
         })
       }
-           
-      if(useKuromoji){
+
+      if (useKuromoji) {
         tokens = tokenizer.tokenize(subdata)
         tokens.forEach(word => {
           delete a[word.surface_form]
@@ -49,10 +48,10 @@ function run () {
       delete a['\n']
       const results = Object.keys(a)
       difference.val(results.join(', '))
-      $('#difference').css('background-color','#fff')
-    });
-  });
-   
+      $('#difference').css('background-color', '#fff')
+    })
+  })
+
   /*
   // URLやデータファイルをDrag&Dropで指定する予定 (未実装 2022/5/23)
   minuendlist = $('#minuendlist')
@@ -64,13 +63,13 @@ function run () {
   //minuend.on("dragover", e => { return false } ) 不要っぽい
   //minuend.on("dragend", e => { return false; } )
   minuend.on('drop', e => {
-	e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
-	//console.log(e.originalEvent.dataTransfer)
-	let dt = e.originalEvent.dataTransfer
-	let li = $('<li>').appendTo(minuendlist)
-	let a = $('<a>').appendTo(li)
-	a.attr('class','list')
-    if(dt.files[0]){ // ファイルのD&D
+    e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
+    //console.log(e.originalEvent.dataTransfer)
+    let dt = e.originalEvent.dataTransfer
+    let li = $('<li>').appendTo(minuendlist)
+    let a = $('<a>').appendTo(li)
+    a.attr('class','list')
+    if(dt.files[0]) { // ファイルのD&D
       a.text(dt.files[0].name)
       a.attr('href','http://example.com')
     }
@@ -80,13 +79,13 @@ function run () {
       a.attr('href',url)
     }
   })
-    
-  $('body').on('drop', function(e){
+
+  $('body').on('drop', function (e) {
     e.preventDefault();  //  デフォルトの「ファイルを開く」処理を抑制
   })
   */
 }
 
-$(function() {
+$(function () {
   run()
 })
