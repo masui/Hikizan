5fa6709976a2a12ed4944137559e5f8f5dd7e403
Toshiyuki Masui <masui@pitecan.com>
2022-06-08 23:52:42 +0900

    ...

--- hikizan.js
+++ hikizan.js
@@ -10,12 +10,34 @@ function run () {
     $('#difference').css('background-color', '#eee')
     const a = {}
     const segmenter = new TinySegmenter()
-    for (const word of segmenter.segment(minuend.val())) {
+  
+    let s = minuend.val()
+    
+    function replacer1(match) {
+        a[match] = 1
+        return(' ')
+    }
+    while(s.match(/\d\d+/)){
+        s = s.replace(/\d\d+/,replacer1)
+    }
+    
+    for (const word of segmenter.segment(s)) {
       a[word] = 1
     }
-    for (const word of segmenter.segment(subtrahend.val())) {
+    
+    s = subtrahend.val()
+    function replacer2(match) {
+        delete a[match]
+        return(' ')
+    }
+    while(s.match(/\d\d+/)){
+        s = s.replace(/\d\d+/,replacer2)
+    }
+    
+    for (const word of segmenter.segment(s)) {
       delete a[word]
     }
+    
     delete a['*']
     delete a[' ']
     delete a['\n']
