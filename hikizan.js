add637636d2d4a013d0b705af1fa6929f8cd95ac
Toshiyuki Masui <masui@pitecan.com>
2023-08-09 15:53:43 +0900

    データ交換を追加

--- hikizan.js
+++ hikizan.js
@@ -1,37 +1,30 @@
-let minuend, subtrahend, difference
+var minuend, subtrahend, difference
 
-function run () {
-  minuend = $('#minuend') // 減算されるデータのtextarea
-  subtrahend = $('#subtrahend') // 減算するデータのtextarea
-  difference = $('#difference') // 減算結果(差分)を表示するtextarea
-
-  // 差分領域をクリックすると差分計算してリストを表示する
-  difference.on('click', () => {
-    $('#difference').css('background-color', '#eee')
+function diff(){
     const a = {}
     const segmenter = new TinySegmenter()
-
+    
     let s = minuend.val()
     // 数字の並びをTinySegmenterから除く
     while (s.match(/\d+/)) {
-      s = s.replace(/\d+/, (w) => {
-        a[w] = 1
-        return ' '
-      })
+	s = s.replace(/\d+/, (w) => {
+            a[w] = 1
+            return ' '
+	})
     }
     for (const w of segmenter.segment(s)) {
-      a[w] = 1
+	a[w] = 1
     }
-
+    
     s = subtrahend.val()
     while (s.match(/\d+/)) {
-      s = s.replace(/\d+/, (w) => {
-        delete a[w]
-        return ' '
-      })
+	s = s.replace(/\d+/, (w) => {
+            delete a[w]
+            return ' '
+	})
     }
     for (const w of segmenter.segment(s)) {
-      delete a[w]
+	delete a[w]
     }
     delete a['*']
     delete a[' ']
@@ -40,10 +33,31 @@ function run () {
     delete a['」']
     const results = Object.keys(a)
     difference.val(results.join(', '))
-    $('#difference').css('background-color', '#fff')
-  })
+}
+
+function run () {
+    minuend = $('#minuend') // 減算されるデータのtextarea
+    subtrahend = $('#subtrahend') // 減算するデータのtextarea
+    difference = $('#difference') // 減算結果(差分)を表示するtextarea
+    
+    $('#exchange').on('click',function(){
+	let v = minuend.val()
+	minuend.val(subtrahend.val())
+	subtrahend.val(v)
+	diff()
+    })
+    
+    minuend.change(diff)
+    subtrahend.change(diff)
+    
+    // 差分領域をクリックすると差分計算してリストを表示する
+    difference.on('click', () => {
+	difference.css('background-color', '#eee')
+	diff()
+	difference.css('background-color', '#fff')
+    })
 }
 
 $(function () {
-  run()
+    run()
 })
